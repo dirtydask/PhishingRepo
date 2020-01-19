489eda055539d7585e5933297526290eee1a38ed
dirtydask <33028676+dirtydask@users.noreply.github.com>
2020-01-19 20:15:24 +0100

    Create setup.sh

---
+++ gophish/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download gophish binary
+wget https://github.com/gophish/gophish/releases/download/v0.8
+.0/gophish-v0.8.0-linux-64bit.zip
+
+# Unzip package
+unzip gophish-v0.8.0-linux-64bit.zip
